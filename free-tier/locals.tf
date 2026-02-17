1cc9048f5f1b3717d5774dc8299927e4f87a425e
ianlmk <ian.lamottekerr@gmail.com>
2026-02-17 00:29:43 -0800

    feat: add comprehensive opentofu-infrastructure IAM policy for full resource management

--- free-tier/locals.tf
+++ free-tier/locals.tf
@@ -112,6 +112,191 @@ locals {
         ]
       })
     }
+    opentofu_infrastructure = {
+      user        = "opentofu"
+      policy_name = "opentofu-infrastructure"
+      policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+          {
+            Sid    = "EC2InstanceManagement"
+            Effect = "Allow"
+            Action = [
+              "ec2:RunInstances",
+              "ec2:TerminateInstances",
+              "ec2:StopInstances",
+              "ec2:StartInstances",
+              "ec2:RebootInstances",
+              "ec2:DescribeInstances",
+              "ec2:DescribeInstanceStatus",
+              "ec2:GetConsoleOutput",
+              "ec2:ModifyInstanceAttribute",
+              "ec2:AttachNetworkInterface",
+              "ec2:DetachNetworkInterface"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "EC2VolumeManagement"
+            Effect = "Allow"
+            Action = [
+              "ec2:CreateVolume",
+              "ec2:DeleteVolume",
+              "ec2:DescribeVolumes",
+              "ec2:DescribeVolumeStatus",
+              "ec2:AttachVolume",
+              "ec2:DetachVolume",
+              "ec2:ModifyVolume",
+              "ec2:ModifyVolumeAttribute"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "ElasticIPManagement"
+            Effect = "Allow"
+            Action = [
+              "ec2:AllocateAddress",
+              "ec2:ReleaseAddress",
+              "ec2:DescribeAddresses",
+              "ec2:AssociateAddress",
+              "ec2:DisassociateAddress",
+              "ec2:ModifyAddressAttribute"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "VPCNetworkManagement"
+            Effect = "Allow"
+            Action = [
+              "ec2:CreateVpc",
+              "ec2:DeleteVpc",
+              "ec2:DescribeVpcs",
+              "ec2:ModifyVpcAttribute",
+              "ec2:DescribeVpcAttribute",
+              "ec2:CreateSubnet",
+              "ec2:DeleteSubnet",
+              "ec2:DescribeSubnets",
+              "ec2:ModifySubnetAttribute",
+              "ec2:CreateInternetGateway",
+              "ec2:DeleteInternetGateway",
+              "ec2:DescribeInternetGateways",
+              "ec2:AttachInternetGateway",
+              "ec2:DetachInternetGateway",
+              "ec2:CreateNatGateway",
+              "ec2:DeleteNatGateway",
+              "ec2:DescribeNatGateways",
+              "ec2:CreateRoute",
+              "ec2:DeleteRoute",
+              "ec2:ReplaceRoute",
+              "ec2:DescribeRouteTables",
+              "ec2:CreateRouteTable",
+              "ec2:DeleteRouteTable",
+              "ec2:AssociateRouteTable",
+              "ec2:DisassociateRouteTable"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "SecurityGroupManagement"
+            Effect = "Allow"
+            Action = [
+              "ec2:CreateSecurityGroup",
+              "ec2:DeleteSecurityGroup",
+              "ec2:DescribeSecurityGroups",
+              "ec2:DescribeSecurityGroupRules",
+              "ec2:AuthorizeSecurityGroupIngress",
+              "ec2:RevokeSecurityGroupIngress",
+              "ec2:AuthorizeSecurityGroupEgress",
+              "ec2:RevokeSecurityGroupEgress",
+              "ec2:CreateSecurityGroupRule",
+              "ec2:DeleteSecurityGroupRule",
+              "ec2:ModifySecurityGroupRules"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "RDSDatabaseManagement"
+            Effect = "Allow"
+            Action = [
+              "rds:CreateDBInstance",
+              "rds:DeleteDBInstance",
+              "rds:DescribeDBInstances",
+              "rds:ModifyDBInstance",
+              "rds:StopDBInstance",
+              "rds:StartDBInstance",
+              "rds:RebootDBInstance",
+              "rds:CreateDBSubnetGroup",
+              "rds:DeleteDBSubnetGroup",
+              "rds:DescribeDBSubnetGroups",
+              "rds:ModifyDBSubnetGroup",
+              "rds:DescribeDBSnapshots",
+              "rds:CreateDBSnapshot",
+              "rds:DeleteDBSnapshot"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "Route53Management"
+            Effect = "Allow"
+            Action = [
+              "route53:CreateHostedZone",
+              "route53:DeleteHostedZone",
+              "route53:DescribeHostedZone",
+              "route53:ListHostedZones",
+              "route53:ListResourceRecordSets",
+              "route53:ChangeResourceRecordSets",
+              "route53:GetChange"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "S3Management"
+            Effect = "Allow"
+            Action = [
+              "s3:CreateBucket",
+              "s3:DeleteBucket",
+              "s3:ListBucket",
+              "s3:GetBucketVersioning",
+              "s3:PutBucketVersioning",
+              "s3:GetBucketEncryption",
+              "s3:PutBucketEncryption",
+              "s3:GetBucketPublicAccessBlock",
+              "s3:PutBucketPublicAccessBlock",
+              "s3:GetObject",
+              "s3:PutObject",
+              "s3:DeleteObject",
+              "s3:ListBucketVersions",
+              "s3:GetBucketLifecycle",
+              "s3:PutBucketLifecycle"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "KMSManagement"
+            Effect = "Allow"
+            Action = [
+              "kms:Decrypt",
+              "kms:GenerateDataKey",
+              "kms:CreateGrant",
+              "kms:DescribeKey",
+              "kms:ListAliases"
+            ]
+            Resource = "*"
+          },
+          {
+            Sid    = "IAMReadOnly"
+            Effect = "Allow"
+            Action = [
+              "iam:ListUsers",
+              "iam:GetUser",
+              "iam:ListAccessKeys",
+              "iam:ListAttachedUserPolicies"
+            ]
+            Resource = "*"
+          }
+        ]
+      })
+    }
   }
 }
 
